f125db275309b14a34df52f6c62aaf1e60d980b8
Ahri <ahri.public@gmail.com>
2023-12-04 19:47:08 +0900

    Work Part Update

--- DeepCheeseBacon/Work/WorkMasterForm.cs
+++ DeepCheeseBacon/Work/WorkMasterForm.cs
@@ -6,10 +6,12 @@ namespace deepcheesebacon
     public partial class WorkMasterForm : Form
     {
         DBHandlerForWork db;
+        Log log;
         public WorkMasterForm()
         {
             InitializeComponent();
             InitDatabase();
+
             LoadCategoryList();
         }
 
@@ -17,6 +19,7 @@ namespace deepcheesebacon
         private void InitDatabase()
         {
             db = DBHandlerForWork.Instance;
+            log = Log.Instance;
         }
 
         /* ----- Category List Load ----- */
@@ -30,11 +33,18 @@ namespace deepcheesebacon
         }
 
         /* ----- Insert Category ----- */
-        private void CategoryRegister_Click(object sender, EventArgs e)
+        private void CategoryRegister_Click_1(object sender, EventArgs e)
         {
             string topCategory = TopCategoryBox.Text;
             string middleCategory = MiddleCategoryBox.Text;
             string bottomCategory = BottomCategoryBox.Text;
+
+            if (topCategory == "" || middleCategory == "" || bottomCategory == "")
+            {
+                log.Here("잘못된 입력입니다.");
+
+                return;
+            }
 
             MySqlDataReader dr = db.Select("EXISTS (SELECT * FROM work_category").Where($"top_category='{topCategory}' AND" +
                 $" middle_category='{middleCategory}' AND bottom_category='{bottomCategory}' LIMIT 1) AS result").Exec();
@@ -42,11 +52,11 @@ namespace deepcheesebacon
             if (dr.Read())
             {
                 string result = dr.GetString("result");
-                dr.Close();
 
+                dr.Close();
                 if (result == "1")
                 {
-                    Log.Instance.Here("Is Exist");
+                    log.Here("Is Exist");
                 }
                 else
                 {
@@ -58,22 +68,47 @@ namespace deepcheesebacon
         }
 
         /* ----- Delete Category ----- */
-        private void CategoryDelete_Click(object sender, EventArgs e)
+        private void CategoryDelete_Click_1(object sender, EventArgs e)
         {
             string categoryId = CategoryID.Text;
-
             bool error = db.Delete("work_category").Where($"category_id={categoryId}").ExecNon();
 
-            if (!error)
+            if (error)
             {
-                Log.Instance.Here("Success");
+                log.Here("CATEGORY DELETE ERROR");
 
-                LoadCategoryList();
+                return;
             }
-            else
+
+            LoadCategoryList();
+        }
+
+        /* ----- Edit Category ----- */
+        private void CategoryEdit_Click(object sender, EventArgs e)
+        {
+            string categoryId = CategoryID.Text;
+            string topCategory = TopCategoryEdit.Text;
+            string middleCategory = MiddleCategoryEdit.Text;
+            string bottomCategory = BottomCategoryEdit.Text;
+
+            if (topCategory == "" || middleCategory == "" || bottomCategory == "")
             {
-                Log.Instance.Here("Fail");
+                log.Here("잘못된 입력입니다.");
+
+                return;
             }
+
+            bool error = db.Update("work_category").Set($"top_category='{topCategory}', " +
+                $"middle_category='{middleCategory}', bottom_category='{bottomCategory}'").Where($"category_id={categoryId}").ExecNon();
+
+            if (error)
+            {
+                log.Here("CATEGORY UPDATE ERROR");
+
+                return;
+            }
+
+            LoadCategoryList();
         }
 
         private void MiddleCategoryEdit_TextChanged(object sender, EventArgs e)
@@ -84,6 +119,50 @@ namespace deepcheesebacon
         private void MiddleCategoryBox_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        /* CategoryID Click */
+        private void CategoryID_Click(object sender, EventArgs e)
+        {
+            LoadCategoryId();
+        }
+
+        private void LoadCategoryId()
+        {
+            MySqlDataReader dr = db.Select("category_id").From("work_category").Exec();
+
+            CategoryID.Items.Clear();
+            while (dr.Read())
+            {
+                CategoryID.Items.Add(dr.GetString("category_id"));
+            }
+
+            dr.Close();
+        }
+
+        /* CategoryID Index Changed */
+        private void CategoryID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadCategoryInfo(CategoryID.Text);
+        }
+
+        private void LoadCategoryInfo(string categoryId)
+        {
+            if (categoryId == "")
+            {
+                return;
+            }
+
+            MySqlDataReader dr = db.Select("*").From("work_category").Where($"category_id={categoryId}").Exec();
+
+            if (dr.Read())
+            {
+                TopCategoryEdit.Text = dr.GetString("top_category");
+                MiddleCategoryEdit.Text = dr.GetString("middle_category");
+                BottomCategoryEdit.Text = dr.GetString("bottom_category");
+            }
+
+            dr.Close();
         }
     }
 }
